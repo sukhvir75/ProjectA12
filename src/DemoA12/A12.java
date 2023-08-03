ac12b748dfa44e7cf00027e2dd312b801b998731
sukhvir75 <140761481+sukhvir75@users.noreply.github.com>
2023-08-03 00:06:21 -0400

    create new class

---
+++ src/DemoA12/A12.java
@@ -0,0 +1,15 @@
+package DemoA12;
+
+public class A12 {
+	public static void main(String args []) {
+		System.out.println("today is monday");
+	}
+	public static void getInfo() {
+		System.out.println("today is java day");
+	}
+	public static void getData() {
+		System.out.println("this is magic");
+	}
+
+
+}
